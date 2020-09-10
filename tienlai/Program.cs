062c93abd76bf717c68982d410e6c1e841b82ac4
ronngo <70925598+ronngo@users.noreply.github.com>
2020-09-10 22:21:27 +0700

    Add files via upload

---
+++ tienlai/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace tienlai
+{
+    class Program
+    {
+        static void tinh()
+        {
+           
+            double money = 1.0;
+            int month = 1;
+            double intersetRate = 1.0;
+
+           
+            Console.WriteLine("Nhap vao tien cho vay: ");
+            money = Double.Parse(Console.ReadLine());
+
+           
+            Console.WriteLine("Nhap vao so thang cho vay: ");
+            month = Int32.Parse(Console.ReadLine());
+
+           
+            Console.WriteLine("Nhap vao ti le lai xuat: ");
+            intersetRate = Double.Parse(Console.ReadLine());
+
+          
+            double totalInterset = 0;
+            for (int i = 0; i < month; i++)
+            {
+                totalInterset = money * (intersetRate / 100) / 12 * 3;
+            }
+          
+            Console.WriteLine("Total of interset: " + totalInterset);
+        }
+        static void Main(string[] args)
+        {
+            tinh();
+        }
+    }
+}
